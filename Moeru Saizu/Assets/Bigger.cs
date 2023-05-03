4a6b0a62a81cb4a755b0de3642283cb2950de3b0
Calum C <calum2.cornelius@live.uwe.ac.uk>
2023-05-03 20:42:00 +0100

    water

--- Moeru Saizu/Assets/Bigger.cs
+++ Moeru Saizu/Assets/Bigger.cs
@@ -7,25 +7,38 @@ public class Bigger : MonoBehaviour
     public GameObject level;
     public GameObject player;
     public GameObject camera;
+    public GameObject water;
     private LvlScript levelScript;
     private CinemachineCameraOffset cameraScript;
     public float tempLvl;
     public float tempZ = -69;
+    public float tempWater = 0.2f;
 
     private void Start()
     {
         levelScript = level.GetComponent<LvlScript>();
         cameraScript = camera.GetComponent<CinemachineCameraOffset>();
         tempLvl = levelScript.lvl;
+        water.SetActive(false);
     }
     private void Update()
     {
-        Debug.Log(tempLvl);
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("mouse pressed");
+            water.SetActive(true);
+        }
+
+        if(Input.GetMouseButtonUp(1))
+        {
+            water.SetActive(false);
+        }
 
         if(levelScript.lvl > tempLvl)
         {
             player.transform.localScale = new Vector3((float)tempLvl + 3, (float)tempLvl + 3, (float)tempLvl + 3);
             cameraScript.m_Offset = new Vector3(0f, 0.7f, tempZ-= 3);
+            water.transform.localScale = new Vector3(tempWater + 0.6f, tempWater + 0.6f, tempWater + 0.6f);
             tempLvl = levelScript.lvl;
         }
     }
